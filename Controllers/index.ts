54dbc15d51db1c3d1d38f7433682bef57d188cf7
WIG216 <numbucliff04@gmail.com>
2023-01-24 09:45:57 +0100

    updates

--- Controllers/index.ts
+++ Controllers/index.ts
@@ -11,26 +11,66 @@
 
 
 
-import userController from "./userController";
-import emailController from "./emailController";
-import authCheckController from "./authCheckController";
+// import userController from "./userController";
+// import authCheckController from "./authCheckController";
 import driverController from "./driverController";
 import vehicleController from "./vehicleController";
 import logsController from "./logsController";
+import driverFreeController from "./driverFreeController";
+import freeRideController from "./freeRideController";
+import * as unassignedController from "./unassignedController";
+import bookingController from "./bookingController";
+import rideController from "./rideController";
 
 
 export default {
    
-    // for class
+    // for vehicle
     getVehiclesController: vehicleController.getVehicle,
     getVehicleByDriverIdController: vehicleController.getVehicleById,
-    createVehicleControler: vehicleController.createVehicle,
+    createVehicleController: vehicleController.createVehicle,
     updateVehicleController: vehicleController.updateAVehicle,
     deleteVehicleController: vehicleController.deleteVehicle,
 
+    // free driver state 
+    getFreeDriverStateController: driverFreeController.getDriversFree,
+    getFreeDriverStateByIdController: driverFreeController.getDriverFreeById,
+    createFreeDriverStateController: driverFreeController.create,
+    updateFreeDriverStateController: driverFreeController.updateDriverFree,
+    deleteFreeDriverStateController: driverFreeController.deleteDriverFree,
+
+    // ride
+    getRidesController: rideController.getRides,
+    getRideByIdController: rideController.getRideByDriverId,
+    createRideController: rideController.createRide,
+    updateRideController: rideController.updateARide,
+    deleteRideController: rideController.deleteRide,
+
+    // booking a ride
+    getBookingController: bookingController.getBooking,
+    getBookingByIdController: bookingController.getBookingById,
+    createBookingController: bookingController.createBooking,
+    updateBookingController: bookingController.updateABooking,
+    deleteBookingController: bookingController.deleteBooking,
+    
+
+    // free ride state 
+    getFreeRideStateController: freeRideController.getFreeRides,
+    getFreeRideStateByIdController: freeRideController.getFreeRideById,
+    createFreeRideStateController: freeRideController.create,
+    updateFreeRideStateController: freeRideController.updateFreeRide,
+    deleteFreeRideStateController: freeRideController.deleteFreeRide,
+
+    // for unassigned
+    getUnassignedController: unassignedController.default.getUnassigned,
+    getUnassignedByIdController: unassignedController.default.getUnassignedById,
+    createUnassignedController: unassignedController.default.createUnassigned,
+    updateUnassignedController: unassignedController.default.updateUnassigned,
+    deleteUnassignedController: unassignedController.default.deleteUnassigned, 
+
    // user
-   createUserController: userController.signUp,
-   loginUserController: userController.login,
+//    createUserController: userController.signUp,
+//    loginUserController: userController.login,
 
    // driver
    createDriverController: driverController.signUp,
@@ -39,17 +79,7 @@ export default {
    // logs
    getLogController: logsController.getLogs,
 
-
-   // for email
-   // for seating
-   getEmailsController: emailController.getEmails,
-   //getSeatingByIdController: seatingController.default.getSeatingById,
-   createEmailControler: emailController.createEmail,
-   updateEmailController: emailController.updateEmail,
-   deleteEmailController: emailController.deleteEmail, 
-   sendMailController: emailController.sendAEmails,
-
    // authCheck 
-   authCheckController: authCheckController.check
+//    authCheckController: authCheckController.check
 }
 
--- Controllers/unassignedController.ts
+++ Controllers/unassignedController.ts
@@ -4,7 +4,7 @@
  *
  * Project: 
  * This is used to boot up our API
- * Created on Tuesday Jan 24 2023
+ * Created on Tuesday Jan 23 2023
 
  */
 
@@ -15,36 +15,22 @@ import messages from "../messages/messages";
 
 import path from "path"
 import mongoosePaginate = require('mongoose-paginate-v2');
-import RideModel from "../Models/rideModel";
-import driversModel from "../Models/driverModel";
+import UnassignedModel from "../Models/unassignedRideModel";
 /**
  * 
- * used to create a Ride
+ * used to create a Unassigned
  *  @param req - request object
  *  @param res - response object
  */
-const createRide = (req: any, res: any) => {
-    let drivers = req.body.driver;
-  
-    RideModel.create(req.body).then((data) => {
-        // adding a ride to a driver
-               drivers.forEach((element:any) => {
-            console.log(element);
-            driversModel.findByIdAndUpdate(element,{$push:{driver:data._id}},{},(departRrr:any, DepartData)=>{
-                
-            })
-        });
-       
-       
+const createUnassigned = (req: any, res: any) => {
+    UnassignedModel.create(req.body).then((data) => {
         res.status(response.CREATED_201);
         res.json({
             success: true,
             docs: data
-        }); 
-    
-    
+        });
     }).catch(err => {
-        res.status(response.OK_200);
+        res.status(response.BAD_REQUEST_400);
         res.json({
             success: false,
             docs: []
@@ -56,37 +42,34 @@ const createRide = (req: any, res: any) => {
 
 /**
  * 
- * used to get Rides
+ * used to get Unassigneds
  *  @param req - request object
  *  @param res - response object
  */
 // 603eb2ee77259abd63745b4d
-const getRides = (req: any, res: any) => {
+const getUnassigned = (req: any, res: any) => {
     const options = {
         page: req.query.page ? req.query.page : 1,
         limit: req.query.limit ? req.query.limit : 10,
     };
-    
-    RideModel.find((err,data)=>{
-      if(!err){
-                 console.log(data)
-            res.status(response.OK_200);
-            res.json({
-                success: true,
-                docs: data
-            });
-      }
-      else{
-        res.status(response.BAD_REQUEST_400);
-                console.log(err)
-                res.json({
-                    success: false,
-                    docs: []
-                })
-      }
-    });
-
-
+    UnassignedModel.find().populate('driver').exec((err,data)=>{
+        if(!err){
+                   console.log(data)
+              res.status(response.OK_200);
+              res.json({
+                  success: true,
+                  docs: data
+              });
+        }
+        else{
+          res.status(response.BAD_REQUEST_400);
+                  console.log(err)
+                  res.json({
+                      success: false,
+                      docs: []
+                  })
+        }
+      });
 };
 
 
@@ -97,14 +80,14 @@ const getRides = (req: any, res: any) => {
  *  @param res - response object
  */
 // 603eb2ee77259abd63745b4d
-const getRideByDriverId = (req: any, res: any) => {
-  let id = req.params.driverId ? req.params.driverId : '';
+const getUnassignedById = (req: any, res: any) => {
+  let id = req.params.id ? req.params.id : '';
   const options = {
       page: req.query.page ? req.query.page : 1,
       limit: req.query.limit ? req.query.limit : 10,
   };
   
-  RideModel.find({driverId:id},(err,data)=>{
+  UnassignedModel.findById(id,(err,data)=>{
     if(!err){
                console.log(data)
           res.status(response.OK_200);
@@ -127,16 +110,15 @@ const getRideByDriverId = (req: any, res: any) => {
 };
 /**
  * 
- * used to update a Ride
+ * used to update a Unassigned
  *  @param req - request object
  *  @param res - response object
  */
-const updateARide = (req: any, res: any) => {
+const updateUnassigned = (req: any, res: any) => {
     let id = req.params.id ? req.params.id : '';
 
     let updateData = req.body;
-
-    RideModel.findOneAndUpdate({ _id: id}, updateData, { new: true }, (err, doc) => {
+    UnassignedModel.findOneAndUpdate({ _id: id}, updateData, { new: true }, (err, doc) => {
         if (err) {
             res.status(response.BAD_REQUEST_400);
             res.json({
@@ -156,19 +138,19 @@ const updateARide = (req: any, res: any) => {
 };
 /**
  * 
- * used to delete a Ride
+ * used to delete a Unassigned
  *  @param req - request object
  *  @param res - response object
  */
- const deleteRide = (req: any, res: any) => {
-    // setting the id of the Ride if passed to {id}
+ const deleteUnassigned = (req: any, res: any) => {
+    // setting the id of the Unassigned if passed to {id}
     let id = req.params.id ? req.params.id : '';
    
-    // deleting the Ride where {id} 
-    RideModel.deleteOne({ _id: id }).then(val => {
-        // Ride deleted
+    // deleting the Unassigned where {id} 
+    UnassignedModel.deleteOne({ _id: id }).then(val => {
+        // Unassigned deleted
         let docCount = val.deletedCount;
-        let responsMessage = docCount ? "Deleted ride" : "Ride Not found";
+        let responsMessage = docCount ? "Deleted document" : "Document Not found";
         res.status(response.OK_200);
         res.json({
             success: true,
@@ -177,22 +159,23 @@ const updateARide = (req: any, res: any) => {
         })
       
     }).catch(err => {
-        // Ride not deleted
+        // Unassigned not deleted
         res.status(response.NO_CONTENT_204);
         res.json({
             success: false,
             message: "Error occured"
         })
     })
-  
-  };
+
+};
+
 
 
 export default {
-  getRides,
-  getRideByDriverId,
-  createRide,
-  updateARide,
-  deleteRide
+  getUnassigned,
+  getUnassignedById,
+  createUnassigned,
+  updateUnassigned,
+  deleteUnassigned
 };
 
